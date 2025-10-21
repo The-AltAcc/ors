1b71160960981d05f43250721aae76ec5ca66428
akiko-npriko <hiddenshadows353@gmail.com>
2025-10-21 00:14:10 -0500

    last commit for now i hope

--- Core.lua
+++ Core.lua
@@ -70,6 +70,7 @@ end
 
 function restoration:Init()
 	restoration.log_shit("SC: LOADING: " .. self.ModPath)
+	restoration:akiko_randomize_unique_units() -- randomize units early :3
 	restoration.captain_types = {
 		winter = {
 			spawn_group = "Cap_Winters",
